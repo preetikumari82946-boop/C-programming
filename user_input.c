c01b5a3ac50426d8690daae1124b124f86a9157d
preetikumari82946-boop <preetikumari82946@gmail.com>
2026-09-05 20:07:06 +0530

    my initial commit

---
+++ user_input.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    //Taking input  from the user
+    int preetiInput;
+    printf("Please enter the value of the preetiInput");
+    scanf("%d",&preetiInput);
+
+    //printf("\n You entered %d as preetiInput",preetiInput);
+
+    printf("\n You entered %f as preetiInput",(float) preetiInput);
+    
+    
+    //TYPE CASTING 
+
+    int num1,num2;
+    return 0;
+}
