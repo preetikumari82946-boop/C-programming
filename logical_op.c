e2b02477ab96cdbba03c7272366b6dfd9b456807
preetikumari82946-boop <preetikumari82946@gmail.com>
2026-09-07 18:56:37 +0530

    my second push

---
+++ logical_op.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+
+//Logical operators
+ int p=0,j=1;
+ printf("The logical operator returned %d\n",p&& j);
+ printf("The logical operator returned %d\n",p || j);
+ printf("The logical operator returned %d\n",!j);
+ printf("The logical operator returned %d\n",! p);
+
+ return 0;
+}
