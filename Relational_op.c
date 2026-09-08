59d518031ac8da7972881592dd57172749457227
preetikumari82946-boop <preetikumari82946@gmail.com>
2026-09-08 18:07:14 +0530

    my initial commit

---
+++ Relational_op.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    //Relational operatoprs
+int preeti=9,mahi=45;
+printf("\n %d ",preeti==mahi);
+printf("\n %d ",preeti!=mahi);
+printf("\n %d ",preeti>=mahi);
+printf("\n %d ",preeti<=mahi);
+
+return 0;
+}
