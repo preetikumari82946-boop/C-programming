3fbd16f408a248b12363d60fb69273fff2cad868
preetikumari82946-boop <preetikumari82946@gmail.com>
2026-09-09 19:09:29 +0530

    my initial commit

---
+++ bitwise_op.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    //Bitwise operator
+ int A=60;
+ int B=70;
+ printf("bitwise and operator returned %d\n",A&B);
+printf("bitwise or operator returned %d\n",A|B);
+printf("bitwise xor operator returned %d\n",A^B);
+printf("bitwise left shift operator returned %d\n",A<<B);
+printf("bitwise right shift operator returned %d\n",A>>B);
+
+return 0;
+}
