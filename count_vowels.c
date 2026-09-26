515df79ce13f13e295d6e5efb3149c2b90d7baa9
preetikumari82946-boop <preetikumari82946@gmail.com>
2026-09-26 20:47:43 +0530

    my initial commit

---
+++ count_vowels.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == 'a' || str[i] == 'e' ||
+           str[i] == 'i' || str[i] == 'o' ||
+           str[i] == 'u')
+        {
+            count++;
+        }
+    }
+
+    printf("Vowels = %d", count);
+
+    return 0;
+}
